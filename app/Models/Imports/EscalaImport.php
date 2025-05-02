2dd4cad9a59e1fa9570b85bbe3127d933b7acf9b
Newton Gonzaga Costa <ncosta.corp@gmail.com>
2025-05-01 22:51:43 -0300

    resolvendo conflito no escalamport

--- app/Models/Imports/EscalaImport.php
+++ app/Models/Imports/EscalaImport.php
@@ -3,25 +3,12 @@
 namespace App\Models\Imports;
 
 use Maatwebsite\Excel\Concerns\ToArray;
-use Illuminate\Support\Facades\Log;
-<<<<<<< Updated upstream
-use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
 
-=======
-use Maatwebsite\Excel\Concerns\WithStartRow;
->>>>>>> Stashed changes
 class EscalaImport implements ToArray
 {
 
-	// public function startRow(): int
-    // {
-    //     return 3;
-	// }
-
-	public function array(array $row) {
+	public function array(array $row)
+	{
 		return $row;
-		// return [
-		// 	'a'=>1
-		// ];
 	}
 }
